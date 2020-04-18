8dfc0f1ac931f01b05d92cb8d1af0ed6131b953d
Mukul More <mukul.more@theviralfever.com>
2020-04-18 10:01:27 +0530

    ADD: Project initialized

---
+++ NewsStream/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsStream
+//
+//  Created by Mukul More on 18/04/20.
+//  Copyright © 2020 Mukul More. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
